1351ef9eb803ba098a04140f449db90e52e672e5
Charles Robinson <the.real.robinsons@gmail.com>
2021-01-07 12:51:05 -0800

    Added orm.js file to the config folder. Created models folder in main folder and added burger.js file to it.

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+const connection = require("./connection");
+
+//  Create 3 ORM methods:
+//  `selectAll()`
+//  `insertOne()`
+//  `updateOne()`
+
+// export orm object
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,5 @@
+const orm = require("../config/orm");
+
+// Inside here create the code that will call the ORM functions using burger specific input for the ORM.
+
+// Export at the end
