732a2b5dfe9f1237ed4343025e1cd40fe4662be5
Charles Robinson <the.real.robinsons@gmail.com>
2021-01-07 11:01:28 -0800

    Created gitignore file for node modules and created password for mysql module and added it to the gitignore file.

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(55) UNIQUE NOT NULL,
+    devoured BOOLEAN default false
+)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers_db.burgers(burger_name)
+VALUES  ('Cheese Burger'),
+        ('Maui Burger'),
+        ('Double Double In-N-Out Burger');
